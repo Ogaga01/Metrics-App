4697815d726be6a3bddfff4d6692d1048089ee6f
Ogaga <iyaraogaga69@gmail.com>
2022-06-22 11:22:21 +0100

    Add search section

---
+++ src/Components/Forms.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Form = ({ onChange }) => (
+  <div className="form-container d-flex">
+    <form>
+      <input type="text" placeholder="Search Country" onChange={onChange} />
+    </form>
+  </div>
+);
+
+Form.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Form;
---
+++ src/Components/GlobalHoliday.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AiOutlineGlobal } from 'react-icons/ai';
+
+const GlobalHoliday = () => (
+  <div className="global-details">
+    <div className="globe">
+      <AiOutlineGlobal className="globe-icon" size={80} />
+    </div>
+
+    <div className="project-title">
+      <p>Global Holidays</p>
+    </div>
+  </div>
+);
+
+export default GlobalHoliday;
