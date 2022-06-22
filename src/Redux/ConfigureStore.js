cafdfc20215a80a44a5bc0100ee8298a227be2a8
Ogaga <iyaraogaga69@gmail.com>
2022-06-22 11:09:26 +0100

    Installed Logger and Configured store

--- src/Redux/ConfigureStore.js
+++ src/Redux/ConfigureStore.js
@@ -1,9 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import countrysReducer from './Country';
+import holidayReducer from './Holiday';
 
-const rootReducers = combineReducers({
-
+const reducer = combineReducers({
+  country: countrysReducer,
+  holiday: holidayReducer,
 });
-const store = createStore(rootReducers, applyMiddleware(thunk));
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger, thunk),
+);
 
 export default store;
