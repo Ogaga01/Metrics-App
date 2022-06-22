acfbf129d4ff4b6ea73014728ea721a01c9a5453
Ogaga <iyaraogaga69@gmail.com>
2022-06-22 11:27:19 +0100

    Add country list

---
+++ src/Components/CountryList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CountryTiles from './CountryCard';
+
+const CountryList = () => (
+  <div className="country-container">
+    <CountryTiles />
+  </div>
+);
+
+export default CountryList;
