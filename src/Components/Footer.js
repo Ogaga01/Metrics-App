a139c8735614359b1505e643d3ac1b98d0fd5084
Ogaga <iyaraogaga69@gmail.com>
2022-06-22 11:36:23 +0100

    Add footer section

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
+
+function Footer() {
+  return (
+    <section className="footer">
+      Copyright ©️
+      <AiFillGithub />
+      <a href="https://github.com/Ogaga01">Ogaga Iyara</a>
+    </section>
+  );
+}
+
+export default Footer;
